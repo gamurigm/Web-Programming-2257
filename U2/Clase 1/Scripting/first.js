59d57b3fc242c467cf8160ee7fffbd2e04007906
gabriel murillo <gamurigm@gmail.com>
2024-12-13 12:21:02 -0500

    U2

---
+++ U2/Clase 1/Scripting/first.js
@@ -0,0 +1,9 @@
+let nombre;
+let edad;
+let altura;
+
+nombre="juan";
+edad=20;
+altua=1.8;
+
+console.log("el nombre de la personaes:", + nombre);
---
+++ U2/Clase 1/Scripting/index.js
@@ -0,0 +1,3 @@
+var userName = "gabriel";
+var nombre2 = "dana";
+console.log("el nombre es: "+userName);
